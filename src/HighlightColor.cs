1c2abeb2a10d4ccf4dfd73a306f6303c0aeeecf8
Matt Lacey <matt@mrlacey.co.uk>
2024-11-26 09:13:31 +0000

    Don't consider supporting Black as a color
    considered as a way to hide code but collapsing enables this too

--- src/HighlightColor.cs
+++ src/HighlightColor.cs
@@ -2,7 +2,6 @@
 
 namespace VSHighlighter;
 
-// TODO: Consider adding black as a way to hide text  - will also need to have that drawn on a layer on top of the text, rather than behind it
 public enum HighlightColor
 {
 	DarkTurquoise,
